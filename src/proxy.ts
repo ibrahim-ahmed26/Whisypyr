bcc75c76a8681ccca8123e6e4ca860c9e5a0f704
ibrahim-ahmed201 <ibrahimahmed384888@gmail.com>
2026-03-15 01:34:09 +0200

    setup the proxy for the unauthroized access and only made sure that it runs only on the crm

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,15 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerClient } from "./app/lib/supabase/server";
+import { URL } from "url";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const supabase = await getServerClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data.user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
 }
+export const config = {
+  matcher: ["/crm", "/crm/:path*"], // make sure only runs this middle ware on these routes
+};
