21ece07e598307274be3bc48df581b4461ad60f0
ibrahim-ahmed201 <ibrahimahmed384888@gmail.com>
2026-03-26 17:24:11 +0200

    get all leads has been done

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -1,4 +1,6 @@
+import { prisma } from "@/app/lib/prisma";
 import { leadServiceSchema } from "@/app/services/lead/schema";
+import { Prisma } from "@/generated/prisma/client";
 import {
   authenticateUser,
   AuthenticationError,
@@ -9,7 +11,7 @@ import { ZodError } from "zod";
 export async function GET(request: NextRequest) {
   // autenticate user
   try {
-    const profile = await authenticateUser;
+    const profile = await authenticateUser([]);
     const searchParams = request.nextUrl.searchParams;
     const page = searchParams.get("page");
     const pageSize = searchParams.get("pageSize");
@@ -19,6 +21,17 @@ export async function GET(request: NextRequest) {
       pageSize,
     });
     // fetch Leads
+
+    // where condition
+    const where: Prisma.LeadWhereInput = {
+      assignToId: profile.id,
+    };
+    const leads = await prisma.lead.findMany({
+      where,
+      take: params.pageSize,
+      skip: (params.page - 1) * params.pageSize,
+    });
+    return NextResponse.json({ data: leads }, { status: 200 });
   } catch (error) {
     // 1- if the error from the authentication user
     if (error instanceof AuthenticationError) {
