b71d56a7f8577880b67f757eaa18a350d2b964fa
Ibrahim Ahmed <ibrahimahmed38488@gmail.com>
2026-04-01 15:51:32 +0200

    setup for the activity table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <div>Home</div>;
+  const router = useRouter();
+  function handleRoute() {
+    router.push("/login");
+    router.refresh();
+  }
+  return (
+    <div className="flex items-center justify-center h-dvh">
+      <Button
+        variant="outline"
+        className="bg-blue-500 cursor-pointer hover:bg-blue-600 hover:text-white text-white transition-all"
+        onClick={handleRoute}
+      >
+        Login
+      </Button>
+    </div>
+  );
 }
