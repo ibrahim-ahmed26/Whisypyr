402049a90a82fcf841a5cc914b8aa20b9685eae9
Ibrahim Ahmed <ibrahimahmed38488@gmail.com>
2026-04-02 13:28:51 +0200

    Handled the Activity Schema

---
+++ src/app/services/activites/schema.ts
@@ -0,0 +1,42 @@
+import { ActivityType, LeadStages, LeadStatus } from "@/generated/prisma/enums";
+import z from "zod";
+const leadStatusSchema = z.enum(LeadStatus);
+const leadStageSchema = z.enum(LeadStages);
+export const createActivitySchema = z
+  .object({
+    leadId: z.uuid(),
+    actorId: z.uuid(),
+    type: z.enum(ActivityType),
+    meta: z
+      .object({
+        to: z.unknown(),
+        from: z.unknown(),
+      })
+      .optional(),
+  })
+  .superRefine((data) => {
+    if (
+      ["STATUS_CHANGE", "STAGE_CHANGE", "ASSIGNMENT_CHANGE"].includes(data.type)
+    ) {
+      if (!data.meta) {
+        throw new Error(`Meta is required for activity type ${data.type}`);
+      }
+      switch (data.type) {
+        case ActivityType.STATUS_CHANGE:
+          data.meta.from = leadStatusSchema.parse(data.meta.from);
+          data.meta.to = leadStatusSchema.parse(data.meta.to);
+          break;
+        case ActivityType.STAGE_CHANGE:
+          data.meta.from = leadStageSchema.parse(data.meta.from);
+          data.meta.to = leadStageSchema.parse(data.meta.to);
+          break;
+        case ActivityType.ASSIGNMENT_CHANGE:
+          data.meta.from = z.string().parse(data.meta.from); // agent name
+          data.meta.to = z.string().parse(data.meta.to); // agent name
+          break;
+        default:
+          break;
+      }
+    }
+  });
+export type CreateActivityInput = z.infer<typeof createActivitySchema>;
